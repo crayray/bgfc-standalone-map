3f8ceb173942ec0adbf8d20e8950f702adeabfae
rachfuerst <rach.fuerst@gmail.com>
2020-08-22 11:14:59 -0500

    Creating instance, installing Google API dependencies, creating page layout

---
+++ src/Components/MapLayout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class MapLayout extends Component {
+    render() {
+        return (
+            <div>
+            <div>
+                <h1>Search for a restaurant:</h1>
+            </div>
+                
+            </div>
+        )
+    }
+}
